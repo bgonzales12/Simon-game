c938033b912b9b111fa90f9decbf46f55c20dfab
Brandon Gonzales <brandong3128@gmail.com>
2023-12-02 20:01:44 -0800

    Basic function implementation

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let playerFail = false;
 let simonList = [];
 let playerList = [];
 
+// Dim and change border color of boxes on click
 boxes.on("mousedown", (event) => {
     $(event.target).css("filter", "brightness(50%)");
     $(event.target).css("border-color", accent);
@@ -15,12 +16,17 @@ boxes.on("mouseup", (event) => {
     $(event.target).css("border-color", "black");
 });
 
+// Game starth
 $(document).on("keypress", () => {
     alert("game start");
-    
+    $("#text").text(`Round ${round}`);
+
     for (let i = 1; i <= round; i++) {
         console.log(i);
         simonList.push(Math.ceil(Math.random() * 4));
         console.log(simonList);
     }
+
+
 });
+
